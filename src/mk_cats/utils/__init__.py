d296c797101aa0423318d5f18cbdad1d4b467e8d
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-04-26 03:45:54 +0300

    Update __init__.py

--- src/mk_cats/utils/__init__.py
+++ src/mk_cats/utils/__init__.py
@@ -1,6 +1,6 @@
 from .check_en import check_en_temps
-from .portal_list import portal_en_to_ar_lower
 from .filter_en import filter_cat
+from .portal_list import portal_en_to_ar_lower
 
 __all__ = [
     "portal_en_to_ar_lower",
