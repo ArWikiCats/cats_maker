6b1adf908bdd18bc21bd4b440c6d98ba980234fa
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-04-27 05:27:26 +0300

    Update __init__.py

--- src/core/new_api/__init__.py
+++ src/core/new_api/__init__.py
@@ -1,12 +1,13 @@
 """ """
 
 from .api_utils import botEdit
-from .pagenew import load_main_api
+from .pagenew import load_main_api, load_login_bot
 from .super import ALL_APIS, Login
 
 __all__ = [
     "botEdit",
     "ALL_APIS",
     "load_main_api",
+    "load_login_bot",
     "Login",
 ]
