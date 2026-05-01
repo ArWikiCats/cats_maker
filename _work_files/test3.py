edc5d283dcb617b93b5a00268f597214324d3e16
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-05-01 04:21:00 +0300

    .

--- _work_files/test3.py
+++ _work_files/test3.py
@@ -1,15 +1,15 @@
 """ """
 
+from http.cookiejar import MozillaCookieJar
 import sys
 import logging
 import os
 from pathlib import Path
 
-import json
 import requests
-from requests.cookies import cookiejar_from_dict, RequestsCookieJar
-
 from dotenv import load_dotenv
+import mwclient
+
 
 try:
     load_dotenv()
@@ -32,13 +32,9 @@ session = requests.Session()
 
 session.headers.update({"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"})
 
-cookies_path = Path("C:/Windows/Temp/cookies/wikipedia_ar_mr.ibrahembot.json")
-cookies_data = {}
-if cookies_path.exists():
-    cookies_data = json.load(cookies_path.open("r", encoding="utf-8"))
-    print(f"load cookies data: {len(cookies_data)}")
+cookies_jar_path = "C:/Windows/Temp/cookies/wikipedia_ar_mr.ibrahembot.mozilla"
 
-session.cookies = cookiejar_from_dict(cookies_data)
+session.cookies = MozillaCookieJar(cookies_jar_path)
 
 auth = AuthProvider(
     lang="ar",
@@ -51,3 +47,36 @@ auth = AuthProvider(
 auth.login()
 
 session.cookies.save(ignore_discard=True, ignore_expires=True)
+
+# ==========================
+
+
+_site = mwclient.Site(
+    "ar.wikipedia.org",
+)
+
+cookies_path = "C:/Windows/Temp/cookies/wikipedia_ar_mr.ibrahembot.json"
+
+_site.connection.cookies = requests.sessions.RequestsCookieJar()
+
+_site.login(
+    username=os.getenv("WIKIPEDIA_BOT_USERNAME"),
+    password=os.getenv("WIKIPEDIA_BOT_PASSWORD"),
+    # cookies=auth.cookies
+)
+
+print(f"{_site.logged_in=}")
+
+# ==========================
+
+client = WikiLoginClient_1(
+    lang="ar",
+    family="wikipedia",
+    username=os.getenv("WIKIPEDIA_BOT_USERNAME"),
+    password=os.getenv("WIKIPEDIA_BOT_PASSWORD"),
+)
+
+
+client.login()
+
+# ==========================
